12ccc31ade1bad3c7d14c1f924fb0f071998cfdb
eshan810 <eshanistaken@gmail.com>
2024-09-14 01:23:41 +0530

    sec

--- assign3/codes/o1.py
+++ assign3/codes/o1.py
@@ -16,38 +16,38 @@ from conics.funcs import circ_gen
 def read_points_from_file(filename):
     with open(filename, 'r') as file:
         lines = file.readlines()
-        A = np.array([float(x) for x in lines[0].strip().split()]).reshape(-1, 1)
-        B = np.array([float(x) for x in lines[1].strip().split()]).reshape(-1, 1)
-    return A, B
+        B = np.array([float(x) for x in lines[0].strip().split()]).reshape(-1, 1)
+        A = np.array([float(x) for x in lines[1].strip().split()]).reshape(-1, 1)
+    return B, A
 
 # Read points A and B from file
-A, B = read_points_from_file('output')
+B, A = read_points_from_file('output')
 
 # Ratio for calculating C
 n = 1.5
 
 # Compute the position vector of point C
-C = A + n * (B - A)
+C = B + n * (A - B)
 
 # Print the computed position vector of C
 print(f"Position vector of C: {C.flatten()}")
 
 # Generate line segments
 x_AB = line_gen(A, B)
-x_BC = line_gen(B, C)
+x_AC = line_gen(A, C)
 
 # Plotting the lines
 plt.plot(x_AB[0, :], x_AB[1, :], label='$AB$')
-plt.plot(x_BC[0, :], x_BC[1, :], 'r--', label='$BC$')
+plt.plot(x_AC[0, :], x_AC[1, :], 'r--', label='$AC$')
 
 # Plotting points
-tri_coords = np.block([[A, B, C]])
+tri_coords = np.block([[B, A, C]])
 plt.scatter(tri_coords[0, :], tri_coords[1, :])
 
 # Annotating points without using a for loop
-plt.text(tri_coords[0, 0], tri_coords[1, 0], f'A\n({tri_coords[0, 0]:.0f}, {tri_coords[1, 0]:.0f})',
+plt.text(tri_coords[0, 0], tri_coords[1, 0], f'B\n({tri_coords[0, 0]:.0f}, {tri_coords[1, 0]:.0f})',
          fontsize=9, ha='center', va='center')
-plt.text(tri_coords[0, 1], tri_coords[1, 1], f'B\n({tri_coords[0, 1]:.0f}, {tri_coords[1, 1]:.0f})',
+plt.text(tri_coords[0, 1], tri_coords[1, 1], f'A\n({tri_coords[0, 1]:.0f}, {tri_coords[1, 1]:.0f})',
          fontsize=9, ha='center', va='center')
 plt.text(tri_coords[0, 2], tri_coords[1, 2], f'C\n({tri_coords[0, 2]:.0f}, {tri_coords[1, 2]:.0f})',
          fontsize=9, ha='center', va='center')
@@ -62,4 +62,4 @@ plt.legend(loc='best')
 plt.grid(True)
 plt.axis('equal')
 plt.savefig('../plots/plot.png', format='png', bbox_inches='tight')
-
+plt.show()
